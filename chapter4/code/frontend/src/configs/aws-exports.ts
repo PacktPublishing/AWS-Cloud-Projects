55852d6fadf74f900250b6d4271249b0808e85af
Pedro Santos <39189233+pmbsantos@users.noreply.github.com>
2024-08-13 22:20:32 +0100

    Update aws-exports.ts
    changed amplify config file variable names

--- chapter4/code/frontend/src/configs/aws-exports.ts
+++ chapter4/code/frontend/src/configs/aws-exports.ts
@@ -1,7 +1,7 @@
 export const amplifyConfig = {
-  aws_project_region: 'AWS_REGION',
-  aws_cognito_region: 'AWS_REGION',
-  aws_user_pools_id: 'USER_POOL_ID',
-  aws_user_pools_web_client_id: 'APP_CLIENT_ID',
+  aws_project_region: '[CognitoRegion]',
+  aws_cognito_region: '[CognitoRegion]',
+  aws_user_pools_id: '[UserPoolId]',
+  aws_user_pools_web_client_id: '[UserPoolClientId]',
   };
 
